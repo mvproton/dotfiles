89842bc722cabf3b7c593f276dcdbaa95a7d07b9
Maks <mvproton@gmail.com>
2024-04-05 17:16:51 +0300

    Add mc/add-cursor-on-click binding

--- emacs/init.el
+++ emacs/init.el
@@ -252,7 +252,8 @@
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)))
+         ("C-c C-<" . mc/mark-all-like-this)
+         ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
 
 (use-package display-line-numbers
   :defer t
